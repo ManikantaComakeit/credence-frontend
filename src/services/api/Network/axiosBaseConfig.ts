7e2e7f46e4816b8d5ddd52967115bd8095c4de54
ManikantaComakeit <manikanta.kaspa@comakeit.com>
2023-11-20 16:47:18 +0530

    fixed backendurl

--- src/services/api/Network/axiosBaseConfig.ts
+++ src/services/api/Network/axiosBaseConfig.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 const backendApiAxios:AxiosInstance = axios.create({
-    baseURL:process.env.REACT_APP_BACKEND_URL,
+    baseURL:process.env.BACKEND_URL,
     timeout:5000
 });
 
